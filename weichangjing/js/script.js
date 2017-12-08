7eec7eb04554760f4162cbaf564d101a2ea62d42
dongxiaofei3 <itjy189@163.com>
2017-12-08 10:35:31 +0800

    新增前两屏动画

--- weichangjing/js/script.js
+++ weichangjing/js/script.js
@@ -11,4 +11,14 @@ var swiper = new Swiper('.swiper-container', {
         //设置小按钮是否可以点击
         clickable: true,
     },
+    //Swiper4.x animate动画插件引入
+    on:{
+        init: function(){
+            swiperAnimateCache(this); //隐藏动画元素
+            swiperAnimate(this); //初始化完成开始动画
+        },
+        slideChangeTransitionEnd: function(){
+            swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+        }
+    }
 });
