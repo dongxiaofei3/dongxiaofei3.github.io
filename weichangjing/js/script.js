d0c63fb4833817be2c04820c4c758ca76392c4e2
dongxiaofei3 <itjy189@163.com>
2017-12-11 09:07:46 +0800

    添加音乐播放按钮

--- weichangjing/js/script.js
+++ weichangjing/js/script.js
@@ -12,13 +12,38 @@ var swiper = new Swiper('.swiper-container', {
         clickable: true,
     },
     //Swiper4.x animate动画插件引入
-    on:{
-        init: function(){
+    on: {
+        init: function () {
             swiperAnimateCache(this); //隐藏动画元素
             swiperAnimate(this); //初始化完成开始动画
         },
-        slideChangeTransitionEnd: function(){
+        slideChangeTransitionEnd: function () {
             swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
         }
     }
 });
+
+//音频标签
+var mp3 = document.querySelector("#mp3");
+
+//音乐按钮
+var mp3Btn = document.querySelector("#mp3Btn");
+
+//暂停音乐按钮
+//mp3Btn.style.animationPlayState = 'paused'
+//旋转音乐按钮
+//mp3Btn.style.animationPlayState = 'running'
+
+mp3Btn.onclick = function () {
+    if (mp3.paused) {
+        //开始旋转
+        mp3Btn.style.animationPlayState = 'running';
+        //开始音频
+        mp3.play();
+    } else {
+        //暂停旋转
+        mp3Btn.style.animationPlayState = 'paused';
+        //暂停音频
+        mp3.pause();
+    }
+}
